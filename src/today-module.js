78a6999e8e609e560db8ff78d4129e6766377c47
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2025-02-19 00:26:04 +0100

    Add container for yeets and listener for input

--- src/today-module.js
+++ src/today-module.js
@@ -17,10 +17,34 @@ export function loadTodayYeets() {
   dateContainer.setAttribute("id", "date-container");
   dateContainer.textContent = "February 15, Saturday";
 
+  const yeetsContainer = document.createElement("div");
+  yeetsContainer.setAttribute("id", "yeets-container");
+
   const yeetInput = document.createElement("input");
   yeetInput.setAttribute("id", "yeet-input");
   yeetInput.setAttribute("type", "text");
-  yeetInput.setAttribute('placeholder', '+ Add a new task');
+  yeetInput.setAttribute("placeholder", "+ Add a new task");
 
-  content.append(dateContainer, yeetInput);
+  content.append(dateContainer, yeetsContainer, yeetInput);
 }
+
+function generateYeetCard(yeetTitle) {
+  const yeetsContainer = document.querySelector("#yeets-container");
+
+  const yeetCard = document.createElement("div");
+  yeetCard.classList.add("yeets");
+  yeetCard.textContent = yeetTitle;
+
+  yeetsContainer.append(yeetCard);
+}
+
+import { createYeet, addToYeets } from "./todoYeet";
+document.querySelector("#content").addEventListener("keydown", (event) => {
+  const yeetInput = document.querySelector("#yeet-input");
+  if (yeetInput && event.target === yeetInput && event.key === "Enter") {
+    let yeet = createYeet(yeetInput.value);
+    addToYeets(yeet);
+    generateYeetCard(yeet.title);
+    yeetInput.value = "";
+  }
+});
